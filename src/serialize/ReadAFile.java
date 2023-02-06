08d3874e0321eaee820d6722c79af9f7376a60b3
seongwon <seongwon1290@dsm.hs.kr>
2023-02-06 13:27:03 +0900

    📝 :: 직렬화와 파일 입출력

---
+++ src/serialize/ReadAFile.java
@@ -0,0 +1,25 @@
+package src.serialize;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class ReadAFile {
+    public static void main(String[] args) {
+        try {
+            File myFile = new File("Foo.txt");
+            FileReader fileReader = new FileReader(myFile);
+
+            BufferedReader reader = new BufferedReader(fileReader);
+
+            String line = null;
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            reader.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
